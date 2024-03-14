57f3f5122fcce7c4c67b4beecc50f17959c187cd
Max1d0n <145984420+Max1d0n@users.noreply.github.com>
2024-03-14 10:30:25 +0200

    Pract03

---
+++ Pract3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using  namespace std;
+
+int main()
+{
+    int pairs = 0;
+    int arr[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Enter " << i + 1 << " element: ";
+        cin >> arr[i];
+
+        if (arr[i] % 2 == 0)
+            pairs++;
+    }
+
+    cout << "\nPaired numbers: " << pairs;
+
+    int req;
+    cout << "\nEnter required number";
+    cin >> req;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i] == req)
+        {
+            cout << "Requied number index: " << i;
+            return 0;
+        }
+    }
+
+    cout << "There is no number with the same number as required";
+}
