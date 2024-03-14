4531f4961fd934b80480aea31cced001b2442cd6
Max1d0n <145984420+Max1d0n@users.noreply.github.com>
2024-03-14 11:16:50 +0200

    Pract04

---
+++ Pract04.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+
+    int N;
+
+    int prev = 0;
+
+    cout << "Enter array size: ";
+    cin >> N;
+
+    int* Arr = new int[N];
+
+    cout << "Array: ";
+    for (int i = 0; i < N; i++)
+    {
+        Arr[i] = rand() % 100 + prev;
+        prev = Arr[i];
+
+        cout << Arr[i] << "\n";
+    }
+
+    int req;
+
+    cout << "\n\nEnter required number: ";
+    cin >> req;
+
+    int min = 0;
+    int max = N - 1;
+
+    while (min <= max)
+    {
+        int med = min + (max - 1) / 2;
+
+        if (Arr[med] == req)
+        {
+            cout << "Required index: " << med << "\nRequired Number: " << Arr[med];
+            delete[] Arr;
+            return 0;
+        }
+        else if (Arr[med] < req)
+        {
+            min = med + 1;
+        }
+        else if (Arr[med] > req)
+            max = med - 1;
+    }
+
+    cout << "There is no required number :(";
+
+    delete[] Arr;
+}
